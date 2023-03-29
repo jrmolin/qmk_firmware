0779fb6da3e526671bdc6cb948763858c7ae6ab7
mo <jonathan.molinatto@elastic.co>
2023-03-29 16:14:40 -0400

    fix a misspelling

--- keyboards/splitography/keymaps/moseph_multi/keymap.c
+++ keyboards/splitography/keymaps/moseph_multi/keymap.c
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //                             └──────┴──────┴┴──────┴──────┘
 
     [_GREEN] = LAYOUT(
-        TXBOLT,  QWERTY,  _______, _______, _______, KC_SLCK, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______,
+        TXBOLT,  QWERTY,  _______, _______, _______, KC_SCRL, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______,
         KC_LCTL, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_CAPS, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_ENT ,
         KC_LSFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, TG_NUM,  KC_P0,   KC_P1,   KC_P2,   KC_P3,   XXXXXXX, KC_LGUI,
                                             _______, _______, XXXXXXX, KC_LALT
@@ -238,7 +238,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
                 qwerty();
             }
-            return false;
+            return false;/*
         case KC_QUOT:
             if ((biton32(layer_state) != _BLUE)) {
                 break;
@@ -248,7 +248,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else {
                 unregister_code(KC_SLSH);
             }
-            return false;
+            return false;*/
         case BASE:
             if (record->event.pressed) {
                 base_layer();
